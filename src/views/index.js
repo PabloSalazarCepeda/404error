1f2a0084d40258d865272cc834d204362d8fc834
Pablo Novoa <67236009+PRNovoa@users.noreply.github.com>
2025-11-11 14:00:29 +0100

    Initial commit

--- src/views/index.js
+++ src/views/index.js
@@ -1,10 +1,4 @@
 // demo/src/views/index.js
 export const views = {
-	'/': { templateId: 'view-home', templateUrl: './src/views/home.html' },
-	'/about': { templateId: 'view-about', templateUrl: './src/views/about.html'},
-	'/projects': { templateId: 'view-projects', templateUrl: './src/views/proyectos.html' },
-	'/contact': { templateId: 'view-contact', templateUrl: './src/views/contacto.html' },
 	404: { templateId: 'view-404', templateUrl: './src/views/404.html' },
-  	'/layouts': { templateId: 'view-layouts', templateUrl: './src/views/layouts.html' },
-  	'/components': { templateId: 'view-components', templateUrl: './src/views/tipografia.html' },
 };
